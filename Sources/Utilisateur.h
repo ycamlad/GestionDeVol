61e921f674bbc82a6b0077fe220b718185f0a0b6
ycamlad <79607171+ycamlad@users.noreply.github.com>
2025-07-31 12:09:06 -0400

    Update Utilisateur.h

--- Sources/Utilisateur.h
+++ Sources/Utilisateur.h
@@ -9,6 +9,10 @@
 #include "QString"
 #include <ContratException.h>
 
+/**
+    * \brief Struct contenant un constructeur pour la creation d'un utilisateur
+ * **/
+
 
 struct Utilisateur {
     QString nom;
@@ -19,15 +23,32 @@ struct Utilisateur {
     QString pass;
     int statut;
 
+    /**
+     * \brief Constructeur du struct utilisateur
+     * \param[in] p_nom nom de l'utilisateur
+     * \param[in] p_prenom prenom de l'usager
+     * \param[in] p_nomUtilisateur nom d'usager de l'utilisateur
+     * \param[in] p_nAero nom de l'aéroport associé a l'usager
+     * \param[in] p_role role de l'usager
+     * \param[in] p_pass mot de pas de l'usager
+     * \param[in] p_statut accèss de l'usager
+     * \pre p_nom ne doit pas être vide
+     * \pre p_prenom ne doit pas être vide
+     * \pre p_nomUtilisateur ne doit pas être vide
+     * \pre p_nAero ne doit pas être vide
+     * \pre p_role ne doit pas être vide
+     * \pre p_pass ne doit pas être vide
+     * \pre p_statut ne doit pas être vide
+     * **/
     Utilisateur(const QString& p_nom,const QString& p_prenom ,const QString& p_nomUtilisateur,const QString& p_nAero,
                 const QString& p_role, const QString& p_pass, int p_statut)
             : nom(p_nom), prenom(p_prenom),nomUtilisateur(p_nomUtilisateur),nAero(p_nAero), role(p_role), pass(p_pass), statut(p_statut) {
-        PRECONDITION(!p_nom.isEmpty());
-        PRECONDITION(!p_prenom.isEmpty());
-        PRECONDITION(!p_nomUtilisateur.isEmpty());
-        PRECONDITION(!p_nAero.isEmpty());
-        PRECONDITION(!p_role.isEmpty());
-        PRECONDITION(!p_pass.isEmpty());
+        PRECONDITION(!p_nom.isEmpty())
+        PRECONDITION(!p_prenom.isEmpty())
+        PRECONDITION(!p_nomUtilisateur.isEmpty())
+        PRECONDITION(!p_nAero.isEmpty())
+        PRECONDITION(!p_role.isEmpty())
+        PRECONDITION(!p_pass.isEmpty())
         PRECONDITION(p_statut == 0 || p_statut == 1)
     }
 };
